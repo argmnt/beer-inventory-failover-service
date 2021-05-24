afe1c3bd05fc230337db2f13fbb6c5d41b245706
argmnt <yagizgazibaba1996@hotmail.com>
2021-05-24 23:46:39 +0300

    first commit

---
+++ src/test/java/com/argmnt/inventory_failover/InventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argmnt.inventory_failover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
